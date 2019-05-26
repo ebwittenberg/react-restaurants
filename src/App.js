eeb3b6c9a14f11e62f538054788c88fb93e87326
Eric Wittenberg <ebwittenberg@gmail.com>
2019-05-26 17:02:19 -0400

    abbreviates american

--- src/App.js
+++ src/App.js
@@ -61,8 +61,9 @@ class App extends React.Component {
           <select
             onChange={this._filterType}
           >
-            <option value="american">American</option>
-            <option value="mediterranean">Mediterranean</option>
+
+            <option value="american">Amer</option>
+            <option value="mediterranean">Med</option>
             <option value="mexican">Mexican</option>
           </select>
 
